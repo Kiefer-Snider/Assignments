5102ba863708bbba097c93a2492c078a847b5cdb
Kiefer Snider <kiefer.snider@gmail.com>
2013-05-30 19:22:02 -0400

    Initial Commit

---
+++ Hello.py
@@ -0,0 +1,32 @@
+print "Hello World"
+
+
+if True or False and "Hello World":
+    print "Woah"
+elif True:
+    pass
+else:
+    pass
+
+
+for i in range(len([1, 2])):
+    print i
+
+
+for item in ["hello", "world"]:
+    print item
+
+
+while False:
+    pass
+
+
+i = 9
+
+
+result = raw_input("Find ")
+
+if int(result) == 1:
+    print 'correct'
+
+# < > >= <= !=
