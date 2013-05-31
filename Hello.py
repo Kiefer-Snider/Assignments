72a1ae93d2a370266b909e4cb0759726bcf3b890
Kiefer Snider <kiefer.snider@gmail.com>
2013-05-30 22:31:08 -0400

    Finishes Number Guessing Portion

--- Hello.py
+++ Hello.py
@@ -1,3 +1,5 @@
+'''
+
 print "Hello World"
 
 
@@ -32,3 +34,52 @@ if int(result) == 1:
     print 'correct'
 
 # < > >= <= !=
+'''
+
+
+def clear():
+    print '\n' * 15
+
+
+# Quiz Code
+
+'''print "Welcome to the Great Quiz!!"
+print "Prepare to be amazed and test your Knowledge!!"
+clear()'''
+
+
+# Number Guesser
+def guess():    
+    print 'This is the Random Number Guesser!'
+    print 'You will give me two numbers and then you will'
+    print 'Guess a number between those numbers.'
+    print 'Please enter 2 numbers.'
+
+    num1 = raw_input('Number 1: ')
+    num2 = raw_input('Number 2: ')
+
+    import random
+    for x in range(1):
+        ran_num = random.randint(num1, num2)   
+        answer = raw_input('Please guess the number: ')
+        if answer == ran_num:
+            print 'Correct'
+        elif answer > ran_num:
+            answer = raw_input('Sorry that is a little high, Try Again: ')
+        elif answer < ran_num:
+            answer = raw_input('Sorry that is a little low, Try Again: ')
+        else:
+            pass
+
+
+def guess_game():
+    guess()
+
+    play = raw_input('Would you like to play again (y/n): ')
+
+    if play == 'y':
+        guess()
+    elif play == 'n':
+        pass
+    else:
+        pass
